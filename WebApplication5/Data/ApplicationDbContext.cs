115729012be0d39286e0bf03e9bfe40e67c118c5
Admin <Admin@DESKTOP-3CD9UON>
2019-08-07 10:21:59 +0700

    Add project files.

---
+++ WebApplication5/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication5.Models;
+
+namespace WebApplication5.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+    }
+}
