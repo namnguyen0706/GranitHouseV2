67aa3225101a28d00745139d563a4fe479a926b4
Admin <Admin@DESKTOP-3CD9UON>
2019-08-08 17:24:09 +0700

    Edit View

--- WebApplication5/Areas/Admin/Controllers/ProductTypeController.cs
+++ WebApplication5/Areas/Admin/Controllers/ProductTypeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication5.Data;
+using WebApplication5.Models;
 
 namespace WebApplication5.Areas.Admin.Controllers
 {
@@ -20,5 +21,58 @@ namespace WebApplication5.Areas.Admin.Controllers
         {
             return View(_db.ProductTypes.ToList());
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(productType);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id== null)
+            {
+                return NotFound();
+            }
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,ProductType productType)
+        {
+            if (id != productType.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Update(productType);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+
     }
 }
